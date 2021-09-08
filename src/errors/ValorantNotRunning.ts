17ae0dbeed4b148c9833104e8acdf45bc706f0b3
Igor Wessel <igor_wessel@hotmail.com>
2021-09-07 22:26:33 -0300

    chore: modify stack trace valorantnotrunning error

--- src/errors/ValorantNotRunning.ts
+++ src/errors/ValorantNotRunning.ts
@@ -5,6 +5,7 @@ class ValorantNotRunning extends Error {
         this.name = this.constructor.name;
         this.message = "Unable to locate lockfile! Valorant is running?";
 
+        Error.stackTraceLimit = 2;
         Error.captureStackTrace(this, this.constructor);
     }
 }
