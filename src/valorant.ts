a1b3dd8cddf6341c1a3ef5f494d6e8d7391b5ce8
Igor Wessel <igor_wessel@hotmail.com>
2021-09-12 09:03:12 -0300

    feat: move pvp internal config to valorant class
    Add util function save data returned in JSON
    Because i use data returned to create a interface for response.

--- src/valorant.ts
+++ src/valorant.ts
@@ -1,5 +1,8 @@
 import { Fetch } from "@interfaces/http";
+import { Regions } from "@interfaces/resources";
+
 import { ClientSettingsResponse, ValorantProcessResponse } from "@interfaces/localEndpointResponses";
+import { PvpInternalConfig } from "@interfaces/sharedEndpointResponses";
 
 interface ValorantInterface {
     process(): Promise<ValorantProcessResponse | Record<string, never>>;
@@ -8,9 +11,11 @@ interface ValorantInterface {
 
 class Valorant implements ValorantInterface {
     private readonly _fetch: Fetch;
+    private readonly _region: Regions;
 
-    constructor(fetch: Fetch) {
+    constructor(fetch: Fetch, region: Regions) {
         this._fetch = fetch;
+        this._region = region;
     }
 
     /**
@@ -21,6 +26,17 @@ class Valorant implements ValorantInterface {
      */
     async process(): Promise<ValorantProcessResponse | Record<string, never>> {
         const data = await this._fetch<ValorantProcessResponse>("/product-session/v1/external-sessions", "local");
+
+        return data;
+    }
+
+    /**
+     * Config_FetchConfig
+     *
+     * Get various internal game configuration settings set by Riot
+     */
+    async internalConfig(): Promise<PvpInternalConfig> {
+        const data = await this._fetch<PvpInternalConfig>(`/v1/config/${this._region}`, "shared");
 
         return data;
     }
