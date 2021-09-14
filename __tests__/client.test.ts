a72876190e0dbd0ebd2d69d60527590c4331e915
Igor Wessel <igor_wessel@hotmail.com>
2021-09-14 08:28:13 -0300

    wip: working in client tests

--- __tests__/client.test.ts
+++ __tests__/client.test.ts
@@ -1,25 +1,77 @@
 import ValClient from "@app/client";
+
+import fs from "fs";
+import axios from "axios";
+
+import { mocked } from "ts-jest/utils";
+import { SystemNotSupported } from "@errors/SystemNotSupported";
 import { ValorantNotRunning } from "@errors/ValorantNotRunning";
-import { regions } from "@resources";
+
+import { regions, regionShardOverride, shardRegionOverride } from "@resources";
+
+jest.mock("fs");
+jest.mock("axios");
+jest.mock("axios-cookiejar-support");
+
+const mockedClientVersion = {
+    data: { data: { branch: "blabla", buildVersion: "blabla", version: "3.0.0.3" } },
+};
+
+const mockedLocalRequestToken = { data: { accessToken: "test", subject: "test", token: "test" } };
+
+const mockedRequestToken = {
+    headers: {
+        "set-cookie": ["test", "test", "bla", "bla", "bla"],
+    },
+};
+
+const mockedPutAccessToken = {
+    data: {
+        response: {
+            parameters: {
+                uri: "https://playvalorant.com/opt_in#access_token=teste&scope=openid&iss=https%3A%2F%2Fauth.riotgames.com&id_token=teste&token_type=Bearer&session_state=teste&expires_in=3600",
+            },
+        },
+    },
+};
+
+const mockedEntitlementsToken = {
+    data: {
+        entitlements_token: "token",
+    },
+};
+
+const mockedUserInfo = {
+    data: {
+        sub: "id_of_user",
+    },
+};
+
+const mockedFS = mocked(fs, true);
+const mockedAxios = mocked(axios, true);
 
 let valClient: ValClient;
 
-beforeEach(() => {
-    valClient = new ValClient();
-});
-
 describe("Client", () => {
-    describe("without iniatiate client", () => {
+    describe("Without iniciate client", () => {
+        beforeAll(() => {
+            valClient = new ValClient();
+        });
+
+        afterAll(() => {
+            mockedAxios.create.mockClear();
+        });
+
         test("we can get all regions accepted for client", () => {
             const regionsClient = valClient.allRegions;
 
             expect(regionsClient).toEqual(expect.arrayContaining(regions));
         });
 
-        test("we can get a instance for valorant api", () => {
-            const valorantApi = valClient.valorant_api;
+        test("we can get a instance for valorant api", async () => {
+            mockedAxios.create.mockReturnThis();
 
-            expect(valorantApi.defaults.baseURL).toEqual("https://valorant-api.com/v1");
+            expect(mockedAxios.create).toHaveBeenCalledWith({ baseURL: "https://valorant-api.com/v1" });
         });
 
         test("auth must be null", () => {
@@ -57,18 +109,129 @@ describe("Client", () => {
         });
     });
 
-    describe("init client without pass auth", () => {
-        //TODO: I need to figure out how to test this part so that it doesn't break when I open valorant
-        test("if valorant is not running throw a error ValorantNotRunnig ", () => {
-            expect.assertions(1);
-            return expect(valClient.init()).rejects.toThrow(ValorantNotRunning);
+    xdescribe("Iniciate client without auth", () => {
+        const realPlatform = Object.getOwnPropertyDescriptor(process, "platform");
+
+        beforeAll(() => {
+            valClient = new ValClient();
         });
 
-        test("if valorant is running we can init client without auth", async () => {
-            expect.assertions(1);
-            await valClient.init();
+        afterEach(() => {
+            Object.defineProperty(process, "platform", realPlatform);
 
-            expect(valClient.allRegions).toEqual(expect.arrayContaining([valClient.region.toLowerCase()]));
+            mockedAxios.get.mockClear();
+            mockedFS.readFileSync.mockClear();
+            mockedFS.readFileSync.mockReset();
+        });
+
+        test("is not in windows throw a SystemNotSupported", async () => {
+            Object.defineProperty(process, "platform", {
+                ...Object.getOwnPropertyDescriptor(process, "property"),
+                value: "linux",
+            });
+
+            expect(valClient.init({ region: "br" })).rejects.toThrowError(SystemNotSupported);
+        });
+
+        test("valorant is not running (launcher is logged), throw a ValorantNotRunning", async () => {
+            mockedAxios.get.mockResolvedValueOnce(mockedClientVersion);
+
+            mockedFS.readFileSync.mockImplementationOnce(() => {
+                throw { code: "ENOENT", syscall: "open" };
+            });
+
+            expect(valClient.init({ region: "br" })).rejects.toThrowError(ValorantNotRunning);
+        });
+
+        test("valorant is running, get authorization to made request", async () => {
+            mockedFS.readFileSync.mockReturnValueOnce("name:pid:port:password:protocol");
+
+            /**
+             * First request to client version
+             * Second request to get local token
+             */
+            mockedAxios.get.mockResolvedValueOnce(mockedClientVersion).mockResolvedValueOnce(mockedLocalRequestToken);
+
+            await valClient.init({ region: "br" });
+
+            expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+            expect(mockedAxios.get).toHaveBeenNthCalledWith(1, "/version");
+            expect(mockedAxios.get).toHaveBeenNthCalledWith(2, "https://127.0.0.1:port/entitlements/v1/token");
+        });
+
+        test("client now need to have player and valorant instances, only works in local endpoint", async () => {
+            mockedFS.readFileSync.mockReturnValueOnce("name:pid:port:password:protocol");
+
+            /**
+             * First request to client version
+             * Second request to get local token
+             */
+            mockedAxios.get.mockResolvedValueOnce(mockedClientVersion).mockResolvedValueOnce(mockedLocalRequestToken);
+
+            await valClient.init({ region: "br" });
+
+            expect(mockedAxios.get).toHaveBeenCalledTimes(2);
+            expect(valClient.player).not.toBe(null);
+            expect(valClient.valorant).not.toBe(null);
+        });
+    });
+
+    describe("Iniciate client with auth", () => {
+        beforeAll(() => {
+            valClient = new ValClient();
+        });
+
+        test("we can authenticate", async () => {
+            mockedAxios.get.mockResolvedValueOnce(mockedClientVersion);
+
+            mockedAxios.post
+                .mockResolvedValueOnce(mockedRequestToken)
+                .mockResolvedValueOnce(mockedEntitlementsToken)
+                .mockResolvedValueOnce(mockedUserInfo);
+
+            mockedAxios.put.mockResolvedValueOnce(mockedPutAccessToken);
+
+            await valClient.init({ region: "br", auth: { username: "teste", password: "teste" } });
+
+            expect(mockedAxios.get).toHaveBeenCalledWith("/version");
+
+            expect(mockedAxios.post).toHaveBeenNthCalledWith(
+                1,
+                "https://auth.riotgames.com/api/v1/authorization",
+                {
+                    client_id: "play-valorant-web-prod",
+                    nonce: "1",
+                    redirect_uri: "https://playvalorant.com/opt_in",
+                    response_type: "token id_token",
+                },
+                expect.anything(),
+            );
+
+            expect(mockedAxios.post).toHaveBeenNthCalledWith(
+                2,
+                "https://entitlements.auth.riotgames.com/api/token/v1",
+                {},
+                expect.anything(),
+            );
+
+            expect(mockedAxios.post).toHaveBeenNthCalledWith(
+                3,
+                "https://auth.riotgames.com/userinfo",
+                {},
+                expect.anything(),
+            );
+
+            expect(mockedAxios.put).toHaveBeenCalledWith(
+                "https://auth.riotgames.com/api/v1/authorization",
+                {
+                    username: "teste",
+                    password: "teste",
+                    type: "auth",
+                },
+                expect.anything(),
+            );
+
+            expect(valClient.auth).toMatchObject({ username: "teste", password: "teste" });
         });
     });
 });
