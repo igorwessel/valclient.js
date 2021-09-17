63ed435de00ef85b87115afee3afe691d50068b9
Igor Wessel <igor_wessel@hotmail.com>
2021-09-17 18:00:01 -0300

    feat: export all lib in index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,34 @@
-import ValClient from "@app/client";
+export * from "@app/http";
+export * from "@app/liveGame";
+export * from "@app/player";
+export * from "@app/preGame";
+export * from "@app/pvp";
+export * from "@app/session";
+export * from "@app/store";
+export * from "@app/valorant";
+export * from "@app/auth";
+export * from "@app/client";
+export * from "@app/contracts";
+export * from "@app/group";
 
-export { ValClient };
+export * from "@interfaces/crosshair";
+export * from "@interfaces/group";
+export * from "@interfaces/helpers";
+export * from "@interfaces/http";
+export * from "@interfaces/liveGame";
+export * from "@interfaces/player";
+export * from "@interfaces/preGame";
+export * from "@interfaces/pvp";
+export * from "@interfaces/resources";
+export * from "@interfaces/session";
+export * from "@interfaces/store";
+export * from "@interfaces/valorant";
+export * from "@interfaces/auth";
+export * from "@interfaces/client";
+export * from "@interfaces/contracts";
+
+export * from "@errors/privateInformationJSON64";
+export * from "@errors/systemNotSupported";
+export * from "@errors/valorantNotRunning";
+
+export * from "@resources";
